928a17ffafbe3b16817614770589e096cf502eb9
tatjam <tatjam02@gmail.com>
2024-08-31 17:11:26 +0200

    Untested blinky (using svd wrapper, thx microzig!)

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 A quick and dirty signal generator for the STM32U0 series (because I had a NUCLEO board). Could be easily adapted to other boards with DAC.
 
-You need to have pyOCD installed (`python3 -m pip install -U pyocd`) for flashing. Debug is also supported
-using pyOCD and the included VS Code runner scripts.
+Register maps have been generated using microzig's regz, and are included in the project. To regenerate these files, download the SVD file from `https://github.com/modm-io/cmsis-svd-stm32/`, regz from `https://github.com/ZigEmbeddedGroup/microzig/blob/main/tools/regz`, build the tool and run it `regz [svd] > src/stm32u083.zig`.
 
 Thanks to https://github.com/haydenridd/stm32-baremetal-zig for the example on how to compile Zig into STM32 devices!
--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,30 @@
 const std = @import("std");
+const stm32u083 = @import("stm32u083.zig");
+
+const periph = stm32u083.devices.STM32U083.peripherals;
 
 comptime {
     @import("startup.zig").export_start_symbol();
 }
 
 export fn main() callconv(.C) noreturn {
-    while (true) {}
+    // LED at PA 5
+    periph.RCC.RCC_IOPENR.modify(.{
+        .GPIOAEN = @as(u1, 0x01), // Enable
+    });
+
+    periph.GPIOA.GPIOA_MODER.modify(.{
+        .MODE5 = @as(u2, 0x1), // General purpose output
+    });
+
+    while (true) {
+        const val = periph.GPIOA.GPIOA_ODR.read().OD5;
+        const nval: u1 = if (val == 0) 1 else 0;
+        periph.GPIOA.GPIOA_ODR.modify(.{
+            .OD5 = nval,
+        });
+        for (0..100000) |a| {
+            _ = a;
+        }
+    }
 }
